fbdf887619de0e0b5774a0112ea256c72c5260c5
MitzIgnacio <150250954+MitzIgnacio@users.noreply.github.com>
2026-02-28 23:17:17 +0800

    Spread system, initial color difference code start

--- scenes/bullet/bullet.gd
+++ scenes/bullet/bullet.gd
@@ -1,25 +1,43 @@
 extends CharacterBody2D
 
 @export var SPEED = 200
+@export var spread_degrees: float = 7.0
+@export var max_lifetime: float = 1.5
 
-var dir
+var dir: float = 0.0
 var spawnPos: Vector2
 var spawnRot: float
-var life: float = 0
+var current_life: float = 0.0
+var team: String = ""
+
+var rng = RandomNumberGenerator.new()
 
 func _ready():
 	global_position = spawnPos
 	global_rotation = spawnRot
+	var spread_rads: float = deg_to_rad(spread_degrees)
+	dir = spawnRot + randf_range(-spread_rads, spread_rads)
+	global_rotation = dir
+	
+	color_set()
 	
 func _physics_process(_delta: float) -> void:
+	bullet_movement()
+	check_lifetime(_delta)
+	
+func bullet_movement():
 	velocity = Vector2(0, -SPEED).rotated(dir)
-	check_lifetime()
 	move_and_slide()
 
-func check_lifetime():
-	life +=1
-	if life > 100:
+
+func color_set():
+	if self.team == "red":
+		$Sprite2D.modulate = Color(1.0, 0.0, 0.0, 1.0)
+
+func check_lifetime(delta):
+	current_life += delta
+	if current_life >= max_lifetime:
 		_die()
-		
+
 func _die():
 	self.queue_free()
--- scenes/cannon/cannon.gd
+++ scenes/cannon/cannon.gd
@@ -2,29 +2,49 @@ extends Node2D
 
 @onready var main = get_tree().get_root().get_node("MainGame")
 @onready var projectile = load("res://scenes/bullet/bullet.tscn")
+
+var team = "red"
 var on_cd := false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
 	
-func _physics_process(delta: float) -> void:
-	rotation_degrees += 5
+func _physics_process(_delta: float) -> void:
+	actions()
+	color_set()
 	shoot()
 	
 	
 func actions():
-	pass
+	if team == "red":
+		if Input.is_action_pressed("red_aimleft") and rotation_degrees <= 270:
+			rotation_degrees += 5
+		if Input.is_action_pressed("red_aimright") and rotation_degrees >= 90:
+			rotation_degrees -= 5
+			
+		if rotation_degrees > 270:
+			rotation_degrees = 270
+		if rotation_degrees < 90:
+			rotation_degrees = 90
+	if team == "blue":
+		pass
+
+
 func shoot():
 	if !on_cd:
 		var instance = projectile.instantiate()
 		instance.dir = rotation
 		instance.spawnPos = self.get_node("BulletSpawnLoc").global_position
 		instance.spawnRot = rotation
+		instance.team = self.team
 		main.add_child.call_deferred(instance)
 		on_cd = true
+
+func color_set():
+	if self.team == "red":
+		$Sprite2D.modulate = Color(1.0, 0.0, 0.0, 1.0)
 	
 
-
 func _on_shoot_cd_timeout() -> void:
-	%ShootCD.start(0.1)
+	%ShootCD.start(0.2)
 	on_cd = false
